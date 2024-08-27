de0a83b389b2a507463e4bc2c90bb0484109cceb
christian goddu <rgoddu909@gmail.com>
2024-08-27 19:47:55 -0400

    okay

--- index.js
+++ index.js
@@ -1,62 +1,72 @@
-const inquirer = require(`inquirer`);
-const EmployeeDatabase = require("./lib")
+const inquirer = require('inquirer');
+const EmployeeDatabase = require('./lib');
 
-const employeeDb = new EmployeeDatabase()
+const employeeDb = new EmployeeDatabase();
 
 const startCLI = () => {
     inquirer.prompt({
-        name: `input`,
-        type: `list`,
-        message: `What would you like to do?`,
+        name: 'input',
+        type: 'list',
+        message: 'What would you like to do?',
         choices: [
-            {
-                name: `view all departments`, value: `viewDepartments`
-            },
-            { name: `view all roles`, value: `viewRoles` },
-            { name: `view all employees`, value: `viewEmployees` },
-            { name: `add a department`, value: `addDepartment` },
-            { name: `add a a role`, value: `addRole` },
-            { name: `add an employee`, value: `addEmployee` },
-            { name: `update an employee role`, value: `updateEmployee` }]
+            { name: 'View all departments', value: 'viewDepartments' },
+            { name: 'View all roles', value: 'viewRoles' },
+            { name: 'View all employees', value: 'viewEmployees' },
+            { name: 'Add a department', value: 'addDepartment' },
+            { name: 'Add a role', value: 'addRole' },
+            { name: 'Add an employee', value: 'addEmployee' },
+            { name: 'Update an employee role', value: 'updateEmployee' }
+        ]
     })
         .then((answers) => {
-            console.log(answers);
-            switch (answers.value) {
-                case "viewDepartments":
+            switch (answers.input) {
+                case 'viewDepartments':
                     employeeDb.getAllDepartments()
                         .then(result => {
-                            console.log('\n')
-                            console.table(result.rows)
-
+                            console.log(result);
                         })
-                        .then(() => startCLI())
-                        .catch(err => console.error(err))
+                        .catch(err => console.error('Error fetching departments:', err))
                     break;
-                case "viewRoles":
+                case 'viewRoles':
                     employeeDb.getAllRoles()
                         .then(result => {
-                            console.log('\n')
-                            console.table(result.rows)
+                            console.log('\n');
+                            console.table(result);
                         })
-                        .then(() => startCLI())
-                        .catch(err => console.error(err))
-                    break
+                        .catch(err => console.error('Error fetching roles:', err))
+                    break;
+                case 'viewEmployees':
+                    employeeDb.getAllEmployees()
+                        .then(result => {
+                            console.log('\n');
+                            console.table(result);
+                        })
+                        .catch(err => console.error('Error fetching employees:', err))
+                    break;
+                case 'addDepartment':
+                    // Add implementation for adding a department
+                    break;
+                case 'addRole':
+                    // Add implementation for adding a role
+                    break;
+                case 'addEmployee':
+                    // Add implementation for adding an employee
+                    break;
+                case 'updateEmployee':
+                    // Add implementation for updating an employee role
+                    break;
                 default:
-                    process.exitCode = 1
-                    process.exit()
+                    console.log('Invalid selection');
+                    process.exit(1); // Exit with an error code
             }
-            // if (answers.value === `viewDepartments`) {
-            // }
+            // process.exit(0)
         })
-}
-
+        .catch(err => console.error('Error processing input:', err));
+};
 
 function init() {
-    console.log(employeeDb.db)
-    console.log(employeeDb.getAllRoles().then((res)=> {
-        console.log(res)
-    }))
+    console.log('Initializing...');
     startCLI()
-
 }
-init()
+
+init();
